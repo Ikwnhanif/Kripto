e070fc02f8a1b43b852c38d1a423deb0353518e8
Ikwnhanif <72723353+Ikwnhanif@users.noreply.github.com>
2023-03-20 12:14:45 +0700

    Add files via upload

---
+++ menuuser.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Menu User</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+    nav {
+        margin: 0px;
+    }
+    div {
+        margin: 20px 100px 0px;
+    }
+    a{
+        margin: 5px 5px;
+    }
+    top{
+        margin: 5px 20px 5px;
+    }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        </button>
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="input.php">Input Pengiriman <span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="listpengiriman.php">Lihat Pengiriman</a>
+            </li>
+            </ul>
+    </nav>
+</body>
+</html>
